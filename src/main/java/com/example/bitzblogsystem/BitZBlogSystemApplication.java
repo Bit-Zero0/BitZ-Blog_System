a3c04df9644861f002c7de672c8aedf269b6db62
Fengalex0 <3153366821@qq.com>
2023-06-02 23:34:43 +0800

    初次提交, 测试

---
+++ src/main/java/com/example/bitzblogsystem/BitZBlogSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitzblogsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitZBlogSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitZBlogSystemApplication.class, args);
+	}
+
+}

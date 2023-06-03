d660f44fae7572f02cd428c3f34046e807ce7c3d
Fengalex0 <3153366821@qq.com>
2023-06-03 22:54:10 +0800

    文章类

---
+++ src/main/java/com/example/bitzblogsystem/Entity/ArticleInfo.java
@@ -0,0 +1,23 @@
+package com.example.bitzblogsystem.Entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ArticleInfo {
+    private Integer id;
+    private String title;
+    private String cotent;
+
+    @JsonFormat(pattern = "yyyy-MM-dd" , timezone = "GMT+8")
+    private LocalDateTime createtime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd" , timezone = "GMT+8")
+    private LocalDateTime updatetime;
+
+    private Integer uid;
+    private Integer rcount;
+    private Integer state;
+}

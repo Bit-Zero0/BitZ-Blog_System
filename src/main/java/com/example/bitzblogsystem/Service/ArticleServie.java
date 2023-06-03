0eb92a551e2810845de3a62fd97e577ae322e528
Fengalex0 <3153366821@qq.com>
2023-06-03 22:53:39 +0800

    文章服务类

---
+++ src/main/java/com/example/bitzblogsystem/Service/ArticleServie.java
@@ -0,0 +1,28 @@
+package com.example.bitzblogsystem.Service;
+
+import com.example.bitzblogsystem.Entity.ArticleInfo;
+import com.example.bitzblogsystem.Mapper.ArticleMapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArticleServie {
+
+    @Autowired
+    private ArticleMapper articleMapper;
+
+    public int getArtCountById(Integer id){
+        return articleMapper.getArtCountByUid(id);
+    }
+
+    public List<ArticleInfo> getList(Integer uid){
+        return articleMapper.getList(uid);
+    }
+
+    public int delArt(Integer id, Integer uid){
+        return articleMapper.delArt(id, uid);
+    }
+}

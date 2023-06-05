6816c06c11bd1fc0fec40e3d441269c8f33671c6
Fengalex0 <3153366821@qq.com>
2023-06-05 21:42:14 +0800

    添加文章与修改文章方法调用

--- src/main/java/com/example/bitzblogsystem/Mapper/ArticleMapper.java
+++ src/main/java/com/example/bitzblogsystem/Mapper/ArticleMapper.java
@@ -1,5 +1,6 @@
 package com.example.bitzblogsystem.Mapper;
 
+import com.example.bitzblogsystem.Common.AjaxResult;
 import com.example.bitzblogsystem.Entity.ArticleInfo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -15,4 +16,10 @@ public interface ArticleMapper {
     int delArt(@Param("id")Integer id, @Param("uid")Integer uid);
 
     ArticleInfo getArtDetail(@Param("id") Integer id);
+
+    int incrRCount(@Param("id") Integer id);
+
+    int update(ArticleInfo articleInfo);
+
+    int add(ArticleInfo articleInfo);
 }

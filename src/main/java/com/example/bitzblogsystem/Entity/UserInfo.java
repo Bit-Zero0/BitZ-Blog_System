3de19c8546a8f4a9236e357febfc38496721519c
Fengalex0 <3153366821@qq.com>
2023-06-09 19:48:52 +0800

    将UserInfo使用Serializable进行序列化

--- src/main/java/com/example/bitzblogsystem/Entity/UserInfo.java
+++ src/main/java/com/example/bitzblogsystem/Entity/UserInfo.java
@@ -2,10 +2,12 @@ package com.example.bitzblogsystem.Entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
+// 必须将用户信息进行序列化, 不然无法存入Redis中
 @Data
-public class UserInfo {
+public class UserInfo implements Serializable {
     private Integer id;
     private String username;
     private String password;
